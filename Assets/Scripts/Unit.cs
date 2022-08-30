87882f806ce056e9ccd202181a3c42fefede3faa
lathindu <lathindu2000@gmail.com>
2022-08-30 23:40:53 +0530

    Base Movement and Animations

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -7,6 +7,7 @@ public class Unit : MonoBehaviour
 
 
     private Vector3 targetPosition;
+    private float stoppingDistance = 0.1f;
 
     private void Move(Vector3 targetPosition)
     {
@@ -15,14 +16,19 @@ public class Unit : MonoBehaviour
 
     private void Update()
     {
-        Vector3 moveDirection = (targetPosition -  transform.position).normalized;
 
-        float moveSpeed = 4f;
-        transform.position += moveDirection  * moveSpeed*Time.deltaTime;
-
-        if (Input.GetKeyDown(KeyCode.T))
+        if ( stoppingDistance <Vector3.Distance(transform.position,targetPosition))
         {
-            Move(new Vector3(4, 0, 4));
+            Vector3 moveDirection = (targetPosition -  transform.position).normalized;
+
+            float moveSpeed = 4f;
+            transform.position += moveDirection  * moveSpeed*Time.deltaTime;
+        }
+       
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Move(MouseWorld.GetPosition());
 
         }
     }
